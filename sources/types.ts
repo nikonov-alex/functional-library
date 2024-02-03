6b381c2b764218387a424976c81c20578505925f
Александр <alex@debian.loc>
2024-02-03 04:32:26 +0200

    cons type

--- sources/types.ts
+++ sources/types.ts
@@ -1,11 +1,12 @@
 type Maybe<A> = A | false;
 
 type EmptyList = null;
+type Cons<A> = {
+    first: A,
+    rest: List<A>
+};
 type List<A> =
     EmptyList |
-    {
-        first: A,
-        rest: List<A>
-    };
+    Cons<A>;
 
-export { Maybe, EmptyList, List };
+export { Maybe, EmptyList, List, Cons };
