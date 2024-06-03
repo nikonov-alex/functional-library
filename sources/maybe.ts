2998f55c9703326ef1e75493bf5ea4367ed483b3
Александр <alex@debian.loc>
2024-06-03 23:45:11 +0300

    updateParam

--- sources/maybe.ts
+++ sources/maybe.ts
@@ -1,3 +1,5 @@
+import { Maybe } from "./types";
+
 export const apply = <T>( predicate: { ( a: any ): a is T } ) =>
     <A, B>( a: A | T, modifier: { ( a: A ): B } ): B | T =>
         predicate( a )
@@ -16,3 +18,36 @@ export const append = <T>( predicate: { ( value: any ): value is T } ) => {
     return <A extends {}, F extends string, V>( a: A | T, field: F, value: V | T ): A & Record<F, V> | T =>
         applyInstance( a, checkedA => addInstance( checkedA, field, value ) );
 }
+
+const updateParam = <P extends string, V extends any, S extends { [k: string]: any }>(
+    state: S,
+    stateParam: P,
+    newValue: V
+): S & Record<P, V> =>
+    undefined === state[stateParam] || state[stateParam] !== newValue
+        ? { ... state, [stateParam]: newValue }
+        : state;
+
+export const updateOptional = <P extends string, V extends any, S extends { [k: string]: any }>(
+    state: S | Error,
+    stateParam: P,
+    newValue: Maybe<V>
+): Error | S | S & Record<P, V> =>
+    state instanceof Error
+        ? state
+    : !newValue
+        ? undefined === state[stateParam]
+            ? state
+            : { ... state, [stateParam]: undefined }
+        : updateParam( state, stateParam, newValue );
+
+export const updateRequired = <P extends string, V extends any, S extends { [k: string]: any }>(
+    state: S | Error,
+    stateParam: P,
+    newValue: V | Error
+): Error | S & Record<P, V> =>
+    state instanceof Error
+        ? state
+    : newValue instanceof Error
+        ? newValue
+    : updateParam( state, stateParam, newValue );
