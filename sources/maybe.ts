5b5af9e58491b01b3e0b19edac1ee453d20cf34c
Александр <alex@debian.loc>
2024-06-04 12:45:34 +0300

    error fix

--- sources/maybe.ts
+++ sources/maybe.ts
@@ -1,23 +1,17 @@
 import { Maybe } from "./types";
 
 export const apply = <T>( predicate: { ( a: any ): a is T } ) =>
-    <A, B>( a: A | T, modifier: { ( a: A ): B } ): B | T =>
-        predicate( a )
-            ? a
-            : modifier( a );
+    <A, B>( modifier: { ( a: A ): B } ) =>
+        ( a: A | T ): B | T =>
+            predicate( a )
+                ? a
+                : modifier( a );
 
-export const add = <T>( predicate: { ( value: any ): value is T } ) =>
+export const append = <T>( predicate: { ( value: any ): value is T } ) =>
     <A extends {}, F extends string, V>( a: A, field: F, value: V | T ): A & Record<F, V> | T =>
         predicate( value )
             ? value
             : ( { ... a, [field]: value } ) as A & Record<F, V>;
-
-export const append = <T>( predicate: { ( value: any ): value is T } ) => {
-    const applyInstance = apply( predicate );
-    const addInstance = add( predicate );
-    return <A extends {}, F extends string, V>( a: A | T, field: F, value: V | T ): A & Record<F, V> | T =>
-        applyInstance( a, checkedA => addInstance( checkedA, field, value ) );
-}
 
 const updateParam = <P extends string, V extends any, S extends { [k: string]: any }>(
     state: S,
